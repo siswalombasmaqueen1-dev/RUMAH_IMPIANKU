13cc7099fef334d62c62c660ee38d29905ec5050
siswalombasmaqueen1-dev <siswalombasmaqueen1@gmail.com>
2025-10-07 13:10:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// 1. pilih tombol berdasarkan ID-nya
 const tombolSapa = document.getElementByld('sapaButton');
+// 2. tambahkan 'event listening' untuk akci 'click'
 tombolSapa.addEventListener('click',function(){
+  // 3. jalankan kode ini ketika tombol di-klik
   alert('hayy!mator tengkyu sudah berkunjung!');
 });
